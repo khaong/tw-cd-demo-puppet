61fc8c94c3fe3111aa42e26418f716e9415b54da
Alex Ong <kha.ong@gmail.com>
2011-06-08 17:26:36 +0100

    add download and install go server

--- modules/go/manifests/server.pp
+++ modules/go/manifests/server.pp
@@ -2,6 +2,7 @@ class go::server {
   include go
   
   $go_home = "/etc/go"
+  $go_server_file = "/home/ubuntu/go-server-2.2.0-13083.deb"
   
   File {
     owner => go,
@@ -9,11 +10,22 @@ class go::server {
     mode => 0440,
   }
   
+  exec { "download_cruise_server"
+    unless => "/bin/ls $go_server_file",
+    command => "/usr/bin/wget http://download01.thoughtworks.com/go/2.2/ga/go-server-2.2.0-13083.deb -O $go_server_file"
+  }
+  
+  exec { "install_go_server":
+    command => "/usr/bin/dpkg -i $go_server_file",
+    require => Exec["download_cruise_server"]
+  }
+    
   file {
     "${go_home}/":
       ensure => directory;
     "${go_home}/cruise-config.xml":
       ensure => present,
       source => 'puppet:///modules/go/sample-cruise-config.xml';
+      require => Exec['install_go_server']
   }
 }
