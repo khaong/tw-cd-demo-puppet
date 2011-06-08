a5c47555ff161d723a60786c110f768d098c283a
Alex Ong <kha.ong@gmail.com>
2011-06-08 17:28:50 +0100

    typo

--- modules/go/manifests/server.pp
+++ modules/go/manifests/server.pp
@@ -10,7 +10,7 @@ class go::server {
     mode => 0440,
   }
   
-  exec { "download_cruise_server"
+  exec { "download_cruise_server":
     unless => "/bin/ls $go_server_file",
     command => "/usr/bin/wget http://download01.thoughtworks.com/go/2.2/ga/go-server-2.2.0-13083.deb -O $go_server_file"
   }
