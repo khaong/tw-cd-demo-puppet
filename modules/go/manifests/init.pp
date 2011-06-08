fb377c6abb9fa81955d2e4309e65caf89af414c0
Alex Ong <kha.ong@gmail.com>
2011-06-08 16:28:17 +0100

    added apt-get update exec

--- modules/go/manifests/init.pp
+++ modules/go/manifests/init.pp
@@ -6,6 +6,11 @@ class go {
     source => 'puppet:///modules/go/canonical.list';
   }
   
+  exec {"apt_update":
+    command => "/usr/bin/apt-get update",
+    require => Exec["/etc/apt/sources.list.d/canonical.list"]
+  }
+  
   package {"sun-java6-jdk":
     require => [Exec["apt_update"], File["/etc/apt/sources.list.d/canonical.list"]],
   }
